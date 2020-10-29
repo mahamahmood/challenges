89a41425e618a4b9167693f42a434dba95d7d4cb
Maha Mahmood <>
2020-10-29 19:41:55 -0400

    solved fizzBuzz and capitalize letters

--- javascript-challenges/part_1/main.js
+++ javascript-challenges/part_1/main.js
@@ -57,8 +57,18 @@ console.log(reverseInt(-521));
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
     const strArr = str.toLowerCase().split(' ');
+    // for(let i=0; i < strArr.length; i++) {
+    //     strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1)// take out the first letter from each word and adding the rest of the word
+    // }
+    // return strArr.join(' ');
 
+    ////// solution 2 //////// 
+    return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
 }
+console.log(capitalizeLetters('i love javascript'));
 
 
 
@@ -71,7 +81,20 @@ function maxCharacter(str) {}
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+    for(let i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if(i % 3 === 0) {
+            console.log('Fizz');
+        } else if(i % 5 === 0) {
+            console.log('Buzz');
+        } else{
+            console.log(i);
+        }
+    }
+}
+console.log(fizzBuzz())
 
 
 
