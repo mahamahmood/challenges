13410bbb7171514752905feb514948bbc7a8be7c
Maha Mahmood <>
2020-10-29 21:25:11 -0400

    reviewed useEffect hooks

--- react_hooks/useeffect_review/src/App.js
+++ react_hooks/useeffect_review/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [resourceType, setResourceType] = useState('posts');
+  const[items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+    console.log('render');
+  }, [resourceType]);// only render if in array changes. if empty never render
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
     </div>
   );
+
+  // const [windowWwidth, setWindowWidth] = useState(window.innerWidth)
+
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth)
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize)
+
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize)
+  //   }
+  // }, []);// onMount add event listener
+
+  // return (
+  //   <div>{windowWwidth}</div>
+  // )
+
+
 }
 
 export default App;
