dadadec9e85ffc35e3b49ee0630c474956ce3790
Maha Mahmood <>
2020-11-06 20:14:24 -0500

    reviewed split, join, eval, sets, multi demensional arrays, and some array methods.

--- javascript-challenges/javascript_review/review_part2.js
+++ javascript-challenges/javascript_review/review_part2.js
@@ -78,3 +78,79 @@ console.log(random2);
 //     x--
 // } while (x > 0);
 
+// Split and Join methods
+const myString = 'Hello World';
+const words = myString.split(' ');// split a string into an array
+console.log(words);
+
+const myArray = ['Hello', 'World'];
+const arr = myArray.join(' ');// join an array to a string
+console.log(arr);
+
+// String Activity
+// Switcharoo
+let firstVariable = 'Hello World';
+firstVariable = 42;
+let secondVariable = firstVariable;
+secondVariable = 'string';
+console.log(firstVariable);
+console.log(secondVariable);
+// Combine it
+const yourName = 'Maha';
+console.log('Hello, my name is ' + yourName);
+console.log(`Hello, my name is ${yourName}`);
+
+
+// eval funcation
+// takes a string and evaluates it as a javascript exprssion
+eval('console.log("hello world")');
+
+const num1 = 5;
+const num2 = 7;
+const operator = '+';
+let result;
+
+eval(`result = ${num1} ${operator} ${num2}`);
+console.log(result);
+
+// Sets
+// sets are datatype that doesn't allow duplicates. it's a good tool for removing duplicates from an array
+const myArr1 = [1, 1, 1, 2, 2, 2];
+const mySet = new Set(myArr1);
+const newArr = [...mySet];
+console.log(mySet);
+console.log(newArr);
+
+// MultiDimensional Array
+// an array inside of an array
+const mArray = [1, 2, 3, 4, 5, [6, 7, [8, 9]], [10, 11]];
+console.log(mArray[5][2][1]); // logs 9
+console.log(mArray[6][0]);// logs 10
+console.log(mArray[5][2][0]); // logs 8
+
+// Getting the last and middle element of an array
+const mArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(mArr[mArr.length-1]);// logs last element
+console.log(mArr[Math.floor(myArr.length/2)]);// middle element;
+
+// Array Methods
+const myArray1 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+/// IndexOf method
+console.log(myArray1.indexOf(5));
+
+/// Push and Pop
+//pop removes the last item and returns it
+//push adds an item to the end of an array and returns new lenght of array
+console.log(myArray1.pop(), myArray1);
+console.log(myArray1.push(9), myArray1);
+
+/// Shift and Unshift
+// Shift removes the first item and returns it.
+// Unshift adds an item to the front of an array and retrun the new length of the array
+console.log(myArray1.shift(), myArray1);
+console.log(myArray1.unshift(0), myArray1);
+
+/// Reverse
+// reverse an array
+console.log(myArray1.reverse(), myArray1);
