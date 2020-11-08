baf026ca639b191223b7a5443a476bcd5b7aea08
Maha Mahmood <>
2020-11-08 15:28:47 -0500

    parctising if else

---
+++ javascript-challenges/javascript_review/reveiw_part3_questions.js
@@ -0,0 +1,12 @@
+////// The Farm //////
+// Write code that will print out 'mooo' if the variable animal is equal to cow
+// change your code so that if the variable animal is anything other than a cow, your code should print out 'hey! you're not a cow'
+
+let animal = 'cow';
+if (animal == 'cow') {
+    console.log('mooooo');
+} else {
+    console.log('Hey! You\'re not a cow');
+}
+
+////// Driver's Ed //////
