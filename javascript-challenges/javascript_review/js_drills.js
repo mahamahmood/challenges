5003d774d3997bd4c5b2a197a436967a4001b3e3
Maha Mahmood <>
2020-11-09 13:40:04 -0500

    Fizz Buzz answered

---
+++ javascript-challenges/javascript_review/js_drills.js
@@ -0,0 +1,38 @@
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+// write a loop to print 1-100
+// write if statment to check if num is divisible by 3 and replace by "Fizz" 
+// write if statment to check if num is divisible by 5 and replace by "Buzz"
+// write if statment to check if num is divisible by 3 and 5 replace by "FizzBuzz"
+// otherwise log num
+for(let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log(`${i} FizzBuzz`);
+    } else if(i % 3 == 0) {
+        console.log(`${i} Fizz`);
+    } else if (i % 5 == 0) {
+        console.log(`${i} Buzz`);
+    } else {
+        console.log(i);
+    }
+} 
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+
+////////////////////////////////
+// Palindrome
+////////////////////////////////
+
+////////////////////////////////
+// Sum Array
+////////////////////////////////
+
+////////////////////////////////
+// Rock Paper Scissors
+////////////////////////////////
