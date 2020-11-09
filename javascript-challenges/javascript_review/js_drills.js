432bdc3b56d396f2897b1ac50eba56bc835f1251
Maha Mahmood <>
2020-11-09 13:50:37 -0500

    Wild Wild Life answered

--- javascript-challenges/javascript_review/js_drills.js
+++ javascript-challenges/javascript_review/js_drills.js
@@ -20,6 +20,25 @@ for(let i = 1; i <= 100; i++) {
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// change Plantee's array to reflect her being a year older
+plantee[2] += 1;
+console.log(plantee);
+// change Wolfy's hometown to Gotham City
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+// give D'Art a second hometown by adding 'Hawkins'
+dart.push('Hawkins');
+console.log(dart);
+// Remove "Wolfy" from the wolfy array and replace it with "Gameboy"
+wolfy.shift()
+wolfy.unshift('Gameboy');
+console.log(wolfy);
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
